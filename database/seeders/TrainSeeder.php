cc9277eb093789ef61b9d616869b5786d712b046
Thomas-Basadonne <thommybasa@gmail.com>
2023-03-30 17:43:17 +0200

    creazione model, controller, seeder

---
+++ database/seeders/TrainSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Trains;
+
+class TrainSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i = 0; $i < 50; $i++) {
+            $train = new Train;
+            $train->azienda = $faker->company();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->orario_partenza = $faker->time('H:i:s');
+            $train->orario_arrivo = $faker->time('H:i:s');
+            $train->codice_treno = $faker->numberBetween(100, 999);
+            $train->numero_carrozze = $faker->numberBetween(1, 10);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
+        }
+    }
+}
